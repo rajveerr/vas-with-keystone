e170293a6638ba9f24dabf6e4acbb390bb05aef0
Rajveer Rathore <rajveerr@gmail.com>
2017-02-25 19:11:07 -0500

    Removing bootstrap that came with keystone by default

--- keystone.js
+++ keystone.js
@@ -80,7 +80,7 @@ keystone.set('email tests', require('./routes/emails'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	enquiries: 'enquiries',
+	products: 'products',
 	users: 'users',
 });
 
